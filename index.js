5269cd0d636e2634d09b8140c2f39545af9938bc
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-11-12 18:14:30 +0000

    Added Function for getting RSS latest feed

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const RSSParser = require("rss-parser");
 
 try {
+    const parse = async url => {
+        const feed = await new RSSParser().parseURL(url);
+
+        console.log(feed.title);
+        console.log(`${feed.items[0].title} - ${feed.items[0].link}\n${feed.items[0].contentSnippet}\n\n`);
+    };
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
+  const feed_list = core.getInput('feed_list');;
+  console.log("Parsing " + feed_list);
+
+  parse(feed_list);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
