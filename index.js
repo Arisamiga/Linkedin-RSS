0965667065005ee37c058b7837ac490286b735c1
Aris <64918822+Arisamiga@users.noreply.github.com>
2023-04-14 15:01:13 +0100

    Removed unnecessary libraries + refactored code

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-const core = require("@actions/core");
-const github = require("@actions/github");
-const RSSParser = require("rss-parser");
-const https = require("https");
+import { getInput, setFailed } from "@actions/core";
+import RSSParser from "rss-parser";
+import { request } from "https";
 
 // ---------------------------------------------------------------------------------------------------------------------
 // Generic Node.js API to post on LinkedIn
 // ---------------------------------------------------------------------------------------------------------------------
-const accessToken = core.getInput('ln_access_token');
-const feed_list = core.getInput('feed_list');
-const embed_images = core.getInput('embed_image');
+const accessToken = getInput("ln_access_token");
+const feed_list = getInput("feed_list");
+const embed_images = getInput("embed_image");
 
 // Get LinkedIn ID, i.e. ownerId
 function getLinkedinId(accessToken) {
@@ -93,7 +92,7 @@ function _request(method, hostname, path, headers, body) {
       rejectUnauthorized: false, // WARNING: accepting unauthorised end points for testing ONLY
     };
     let resBody = "";
-    let req = https.request(reqOpts, (res) => {
+    let req = request(reqOpts, (res) => {
       res.on("data", (data) => {
         resBody += data.toString("utf8");
       });
@@ -120,15 +119,25 @@ try {
     const feed = await new RSSParser().parseURL(url);
 
     console.log(feed.title);
-    getLinkedinId(accessToken).then(ownerId => {
-        postShare(accessToken, ownerId, feed.title, feed.items[0].title, feed.items[0].link, embed_images ?? feed.items[0].link).then(r => {
+    getLinkedinId(accessToken)
+      .then((ownerId) => {
+        postShare(
+          accessToken,
+          ownerId,
+          feed.title,
+          feed.items[0].title,
+          feed.items[0].link,
+          embed_images ?? feed.items[0].link
+        )
+          .then((r) => {
             console.log(r); // status 201 signal successful posting
             if (r.status != 201) {
-                core.setFailed("Failed to post on LinkedIn");
+              setFailed("Failed to post on LinkedIn");
             }
-        }).catch(e => console.log(e));
-    }).catch(e => console.log(e));
-    
+          })
+          .catch((e) => console.log(e));
+      })
+      .catch((e) => console.log(e));
     console.log(
       `${feed.items[0].title} - ${feed.items[0].link}\n${feed.items[0].contentSnippet}\n\n`
     );
@@ -138,5 +147,5 @@ try {
 
   parse(feed_list);
 } catch (error) {
-  core.setFailed(error.message);
+  setFailed(error.message);
 }
