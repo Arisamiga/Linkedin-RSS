991e64282ad3ef4749477eedc8524be8e44f59e6
Aris <64918822+Arisamiga@users.noreply.github.com>
2023-06-17 01:25:30 +0100

    Back 3 steps

--- dist/index.js
+++ dist/index.js
@@ -10395,8 +10395,9 @@ function uploadImageLinkedin(accessToken, image, ownerId) {
         const method = "POST";
         const headers = {
           Authorization: "Bearer " + accessToken,
+          "cache-control": "no-cache",
           "X-Restli-Protocol-Version": "2.0.0",
-          "Content-Type": "multipart/form-data",
+          "Content-Type": "image/png",
           "Content-Length": Buffer.byteLength(image),
           "LinkedIn-Version": "202305",
         };
